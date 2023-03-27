d8766ca78db07f50a35bd8a70011b7158bd3c62f
Amirul Shyakir <amshyaswad@gmail.com>
2023-03-27 23:57:03 +0800

    Added admin login function in Auth managed bean

--- LensLease-war/src/java/managedBean/AuthenticationManagedBean.java
+++ LensLease-war/src/java/managedBean/AuthenticationManagedBean.java
@@ -1,6 +1,8 @@
 package managedBean;
 
+import ejb.session.stateless.AdminSessionBeanLocal;
 import ejb.session.stateless.UserSessionBeanLocal;
+import entity.Admin;
 import entity.User;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
@@ -8,6 +10,7 @@ import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import util.exception.AdminNotFoundException;
 import util.exception.InvalidLoginException;
 import util.exception.UserNotFoundException;
 
@@ -15,6 +18,12 @@ import util.exception.UserNotFoundException;
 @SessionScoped
 public class AuthenticationManagedBean implements Serializable {
 
+    @EJB
+    private AdminSessionBeanLocal adminSessionBean;
+    private String adminUsername = null;
+    private String adminPassword = null;
+    private long adminId = -1;
+    
     @EJB
     private UserSessionBeanLocal userSessionBean;
 
@@ -24,13 +33,8 @@ public class AuthenticationManagedBean implements Serializable {
 
     public AuthenticationManagedBean() {
     }
-
+    
     public String login() {
-        //by right supposed to use a session bean to
-        //do validation here
-        //...
-        //simulate username/password
-        
         FacesContext context = FacesContext.getCurrentInstance();
         try {
             User u = userSessionBean.userLogin(getUsername(), getPassword());
@@ -49,6 +53,28 @@ public class AuthenticationManagedBean implements Serializable {
         username = null;
         password = null;
         userId = -1;
+        return "/login.xhtml?faces-redirect=true";
+    }
+
+    public String adminLogin() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        try {
+            Admin a = adminSessionBean.adminLogin(getUsername(), getPassword());
+            setUserId(a.getAdminId());
+            return "/secret/index.xhtml?faces-redirect=true";
+        } catch (AdminNotFoundException | InvalidLoginException ex) {
+            setAdminUsername(null);
+            setAdminPassword(null);
+            setAdminId(-1);
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, ex.getMessage(), "" ));
+            return "/login.xhtml";    
+        }
+    }
+
+    public String adminLogout() {
+        setAdminUsername(null);
+        setAdminPassword(null);
+        setAdminId(-1);
         return "/login.xhtml?faces-redirect=true";
     }
 
@@ -76,6 +102,48 @@ public class AuthenticationManagedBean implements Serializable {
 
     public void setUserId(long userId) {
         this.userId = userId;
+    }
+
+    /**
+     * @return the adminUsername
+     */
+    public String getAdminUsername() {
+        return adminUsername;
+    }
+
+    /**
+     * @param adminUsername the adminUsername to set
+     */
+    public void setAdminUsername(String adminUsername) {
+        this.adminUsername = adminUsername;
+    }
+
+    /**
+     * @return the adminPassword
+     */
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    /**
+     * @param adminPassword the adminPassword to set
+     */
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+
+    /**
+     * @return the adminId
+     */
+    public long getAdminId() {
+        return adminId;
+    }
+
+    /**
+     * @param adminId the adminId to set
+     */
+    public void setAdminId(long adminId) {
+        this.adminId = adminId;
     }
    
 }
